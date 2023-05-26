1abdb61e56b29cf7e5151e49447ef420ca357d62
NoeRamirezElvir <noe.ramirezjcv.edu.hn>
2023-05-25 22:25:12 -0600

    c15

--- ISI_ClINICA/settings.py
+++ ISI_ClINICA/settings.py
@@ -126,7 +126,6 @@ STATIC_URL = 'CLINICA/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "staticfiles/",
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
