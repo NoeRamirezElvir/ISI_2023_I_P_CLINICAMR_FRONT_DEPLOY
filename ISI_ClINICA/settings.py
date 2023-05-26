dce5e593685df0a871f88b0ef22e310a88cd4dab
NoeRamirezElvir <noe.ramirezjcv.edu.hn>
2023-05-25 22:21:32 -0600

    c13

--- ISI_ClINICA/settings.py
+++ ISI_ClINICA/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'CLINICA/static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [
-    BASE_DIR / "CLINICA/static",
+    BASE_DIR / "/staticfiles/",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 if not DEBUG:
